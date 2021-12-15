50cb73c35fc9b97ad3e6dec17e2e908b08697183
~Ganesh Gajavelli <gajavegr@rose-hulman.edu>
2021-12-15 00:16:53 -0500

    actual alternate-history?

--- Homework/Plumbing2/alternate-history.py
+++ Homework/Plumbing2/alternate-history.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 
 shasList = [str(sys.argv[2]),str(sys.argv[4]),str(sys.argv[6])]
-branchName = str(sys.argv[8])
+branchName = str(sys.argv[8])W
 
 parent = subprocess.run(["git", "commit-tree", shasList[0], "-m", "alternative branch"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip().split("\n")
 parent = subprocess.run(["git", "commit-tree", shasList[0], "-m", "alternative begin"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
