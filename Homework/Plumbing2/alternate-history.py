eea1aac8fe1c7fbee4b29b1ac04798b78d9e11fa
~Ganesh Gajavelli <gajavegr@rose-hulman.edu>
2021-12-15 00:13:14 -0500

    alternate history complete

---
+++ Homework/Plumbing2/alternate-history.py
@@ -0,0 +1,13 @@
+import sys
+import subprocess
+
+shasList = [str(sys.argv[2]),str(sys.argv[4]),str(sys.argv[6])]
+branchName = str(sys.argv[8])
+
+parent = subprocess.run(["git", "commit-tree", shasList[0], "-m", "alternative branch"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip().split("\n")
+parent = subprocess.run(["git", "commit-tree", shasList[0], "-m", "alternative begin"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+parent = subprocess.run(["git", "commit-tree", shasList[1], "-p", parent, "-m", "alternative followup"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+parent = subprocess.run(["git", "commit-tree", shasList[2], "-p", parent, "-m", "alternative followup"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+
+refPath = f"refs/heads/{branchName}"
+subprocess.run(["git","update-ref",refPath,parent],stdout=subprocess.PIPE)
